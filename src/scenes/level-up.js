d7f2b0546a6415775719365bda777c4c36b68bd7
Furka Thorne <furka.thorne@gmail.com>
2018-10-03 16:24:45 +0100

    ugly?

--- src/scenes/level-up.js
+++ src/scenes/level-up.js
@@ -1,90 +1,90 @@
-import Scene from "./_super"
+import Scene from './_super'
 
 const STATS = [
-  {
-    key: "str",
-    name: "Strength",
-    label: "Physical prowess",
-    desc: ""
-  },
-  {
-    key: "stam",
-    name: "Stamina",
-    label: "Physical endurance",
-    desc: ""
-  },
-  {
-    key: "dex",
-    name: "Dexterity",
-    label: "Flexibility and grace",
-    desc: ""
-  },
-  {
-    key: "char",
-    name: "Charisma",
-    label: "Persuasiveness",
-    desc: ""
-  },
-  {
-    key: "will",
-    name: "Willpower",
-    label: "Mental fortitude",
-    desc: ""
-  },
-  {
-    key: "prc",
-    name: "Perception",
-    label: "Situational awareness",
-    desc: ""
-  }
+    {
+        key: 'str',
+        name: 'Strength',
+        label: 'Physical prowess',
+        desc: '',
+    },
+    {
+        key: 'stam',
+        name: 'Stamina',
+        label: 'Physical endurance',
+        desc: '',
+    },
+    {
+        key: 'dex',
+        name: 'Dexterity',
+        label: 'Flexibility and grace',
+        desc: '',
+    },
+    {
+        key: 'char',
+        name: 'Charisma',
+        label: 'Persuasiveness',
+        desc: '',
+    },
+    {
+        key: 'will',
+        name: 'Willpower',
+        label: 'Mental fortitude',
+        desc: '',
+    },
+    {
+        key: 'prc',
+        name: 'Perception',
+        label: 'Situational awareness',
+        desc: '',
+    },
 ]
 
 export default class LevelUpScene extends Scene {
-  constructor(game) {
-    super(game)
+    constructor(game) {
+        super(game)
 
-    this.state.choosePrimaryStats()
-    this.stat = null
-  }
+        this.state.choosePrimaryStats()
+        this.stat = null
+    }
 
-  get FSMStates() {
-    return [
-      { name: "choosePrimaryStats", from: "*" },
-      { name: "confirmation", from: "choosePrimaryStats" },
-      { name: "end", from: "confirmation" }
-    ]
-  }
+    get FSMStates() {
+        return [
+            { name: 'choosePrimaryStats', from: '*' },
+            { name: 'confirmation', from: 'choosePrimaryStats' },
+            { name: 'end', from: 'confirmation' },
+        ]
+    }
 
-  choosePrimaryStats() {
-    this.render({
-      text: `You feel stronger — choose which stat to increase.`,
-      responses: STATS.map(stat => ({
-        text: `${stat.name} — "${stat.label}"`,
-        stat: stat,
-        state: "confirmation"
-      }))
-    })
-  }
+    choosePrimaryStats() {
+        this.render({
+            text: `You feel stronger — choose which stat to increase.`,
+            responses: STATS.map(stat => ({
+                text: `${stat.name} — "${stat.label}"`,
+                stat: stat,
+                state: 'confirmation',
+            })),
+        })
+    }
 
-  confirmation(choice) {
-    this.stat = choice.stat
+    confirmation(choice) {
+        this.stat = choice.stat
 
-    this.render({
-      text: `
+        this.render({
+            text: `
         Level up and gain **+2 ${this.stat.name}**?
 
         ${this.stat.desc}`,
-      responses: [
-        { text: "Yes please!", state: "end" },
-        { text: "I've changed my mind", state: "choosePrimaryStats" }
-      ]
-    })
-  }
+            responses: [
+                { text: 'Yes please!', state: 'end' },
+                { text: "I've changed my mind", state: 'choosePrimaryStats' },
+            ],
+        })
+    }
 
-  end() {
-    this.game.player.stored[this.stat.key] += 2
-    this.game.player.stored.lvl += 1
+    end() {
+        this.game.player.stored[this.stat.key] += 2
+        this.game.player.stored.lvl += 1
 
-    super.end()
-  }
+        super.end()
+    }
 }
